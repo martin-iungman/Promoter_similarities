4d67815cb1019d3462dcdf757355c70e58f6a4a8
martin-iungman <martin.iungman@gmail.com>
2022-09-07 12:00:53 -0300

    change folder structure

--- FANTOM5_files.sh
+++
@@ -1,3 +0,0 @@
-parallel -a /home/tinchis/Documents/Promoter\ architecture/Data/FANTOM5_files.txt --jobs 10 wget -P /home/tinchis/Documents/Promoter\ architecture/DataFANTOM5_files/
-#Esto sirve para descargar los links que estan en el archivo de texto, con 10 cores en paralelo, y los guarda en el directorio.
-# revisar si agregar -n
